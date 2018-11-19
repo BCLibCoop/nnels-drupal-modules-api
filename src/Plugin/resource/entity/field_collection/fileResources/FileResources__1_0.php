9a5e3f83d9eb24718321dd4299a5b30be32a6ead
Jonathan Schatz <jonathan.schatz@bc.libraries.coop>
2018-11-18 19:42:33 -0800

    Add WIP FileResources resource endpoint
    Signed-off-by: Jonathan Schatz <jonathan.schatz@bc.libraries.coop>

--- src/Plugin/resource/entity/field_collection/fileResources/FileResources__1_0.php
+++ src/Plugin/resource/entity/field_collection/fileResources/FileResources__1_0.php
@@ -42,26 +42,72 @@ use Drupal\restful\Plugin\resource\ResourceInterface;
 class FileResources__1_0 extends ResourceEntity {
 
   protected function publicFields() {
-//    $public_fields = array(
-//        'type' => array(
-//          'wrapper_method' => 'getBundle',
-//          'wrapper_method_on_entity' => TRUE,
-//        ),
-//      ) + parent::publicFields();
+
     $public_fields = parent::publicFields();
 
-//    $public_fields['type'] = array(
-//      'wrapper_method' => 'getBundle',
-//      'wrapper_method_on_entity' => TRUE
-//    );
     $public_fields['id']['methods'] = array('GET');
+    $public_fields['s3_info']['callback'] =
+      array($this, 'getS3Info');
 
-    $public_fields['s3_path'] = array(
-      'property' => 'field_s3_path'
+    $public_fields['running_time'] = array(
+      'property' => 'field_running_time',
+      'process_callbacks' => array(
+        array($this, 'formatRunTime')
+      )
     );
     $public_fields['format'] = array(
-      'property' => 'field_file_format'
+      'property' => 'field_file_format',
+      'wrapper_method' => 'label',
     );
+    $public_fields['availability'] = array(
+      'property' => 'field_availability_status',
+      'wrapper_method' => 'label',
+    );
+    $public_fields['public_note'] = array(
+      'property' => 'field_description',
+    );
+    $public_fields['narration'] = array(
+      'property' => 'field_performer',
+    );
+
+    # Unset
+    $unset = array('label', 'self');
+    foreach ($unset as $field) {
+      unset($public_fields[$field]);
+    }
+
     return $public_fields;
+  }
+
+  public function getS3Info($data_emw) {
+    module_load_include('inc', 'cals_s3', 'cals_s3.NNELSStreamWrapper.class');
+
+    $stream = new \Drupal\cals_s3\NNELSStreamWrapper;
+    $stream->setUri($data_emw->getWrapper()->field_s3_path->value());
+
+    $map = array(
+      "s3_path_signed" => array(
+        'setter' => 'getExternalUrl',
+      ),
+      "filesize" => array(
+        'setter' => 'get_filesize',
+        'process' => 'format_size',
+      )
+    );
+
+    $output = array();
+
+    foreach ($map as $field_name => $funcs) {
+      $setter = $funcs['setter'];
+      $output[$field_name] = $stream->$setter();
+      if($funcs['process']) $output[$field_name] =
+        call_user_func($funcs['process'], $output[$field_name]);
+  }
+
+    return $output;
+  }
+
+  public function formatRunTime($runtime) {
+    return date('H:i:s', strtotime($runtime));
   }
 }
